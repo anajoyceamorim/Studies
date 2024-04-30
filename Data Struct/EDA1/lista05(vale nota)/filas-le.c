c8ee930cfd95a075988332b45a1b561cb7d4d616
anajoyceamorim <anajoyceguedesamorimdasilva@gmail.com>
2024-04-29 21:46:34 -0300

    filasLE

---
+++ Data Struct/EDA1/lista05(vale nota)/filas-le.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct celula {
+int dado;
+struct celula *prox;
+} celula;
+
+void imprime (celula *le);
+
+celula *enfileira (celula *f, int x);
+int desenfileira (celula *f, int *y);
+
+int main() {
+    celula *head = malloc(sizeof(celula));
+    celula *fn = enfileira(head, 1);
+
+    fn = enfileira(fn, 2);
+
+    imprime(head);
+
+    int* y;
+    desenfileira(fn, *y);
+    printf("%d \n", *y);
+    imprime(head);
+
+    return 0;
+}
+
+void imprime (celula *f) 
+{
+    while( f->prox != NULL)
+    {
+        printf("%d -> ", f->dado);
+        f = f->prox;
+    }
+    printf("NULL\n");
+}
+
+celula *enfileira (celula *f, int x)
+{
+    celula *nova = malloc(sizeof(celula));
+    if (nova == NULL) return NULL;
+
+    nova->prox = f->prox;
+    f->prox = nova;
+    f->dado = x;
+
+    return nova;
+}
+
+int desenfileira (celula* f, int* y)
+{
+    if (f == NULL || f->prox == NULL ) return 0;
+
+    celula* lixo = f->prox;
+
+    *y = f->prox->dado;
+    f->prox = f->prox->prox;
+
+    free(lixo);
+    return 1;
+}
---
+++ Data Struct/EDA1/lista05(vale nota)/filas-vet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct fila {
+int *dados;
+int N, p, u;
+} fila;
+
+int enfileira (fila *f, int x);
+
+int main() {
+
+
+    return 0;
+}
+
+int enfileira (fila *f, int x)
+{
+    
+}
